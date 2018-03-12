e4f68a527b06310a8d8fb36f19d683177d2d4756
abhinavgunwant <abhigunwant@gmail.com>
2018-03-12 15:08:09 +0530

    Revert "added mass upload component"
    This reverts commit b7139140ab8935ede2f74f105a64e8e6d9bfff20.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,7 +74,6 @@ import { AddDriverComponent } from './add-driver/add-driver.component';
 import { DriverService } from './driver.service';
 import { DriverUpdateComponent } from './driver-update/driver-update.component';
 import { DriverData } from './driver-list/driverData';
-import { MassUploadEmployeeComponent } from './mass-upload-employee/mass-upload-employee.component';
 
 
 @NgModule({
@@ -123,8 +122,7 @@ import { MassUploadEmployeeComponent } from './mass-upload-employee/mass-upload-
     CabUpdateComponent,
     AddCabComponent,
     AddDriverComponent,
-    DriverUpdateComponent,
-    MassUploadEmployeeComponent
+    DriverUpdateComponent
     
   ],
   imports: [
