bdcb8899ccfffe3bee507c63c91ae8cbc4c53c79
abhinavgunwant <abhigunwant@gmail.com>
2018-03-12 15:07:30 +0530

    Revert "Revert "added mass upload component""
    This reverts commit a990cc3b5c8ad5cd45ca49b01c6ba577758da2d8, reversing
    changes made to b7139140ab8935ede2f74f105a64e8e6d9bfff20.

---
+++ src/app/mass-upload-employee/mass-upload-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mass-upload-employee',
+  templateUrl: './mass-upload-employee.component.html',
+  styleUrls: ['./mass-upload-employee.component.css']
+})
+export class MassUploadEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
